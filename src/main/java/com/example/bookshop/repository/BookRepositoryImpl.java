54761a922d14ff94bb2179aa878dc930a4293df7
Bohdan <bohdan2704@gmail.com>
2024-02-25 21:38:35 +0200

    added implementations content

--- src/main/java/com/example/bookshop/repository/BookRepositoryImpl.java
+++ src/main/java/com/example/bookshop/repository/BookRepositoryImpl.java
@@ -1,4 +1,50 @@
 package com.example.bookshop.repository;
 
-public class BookRepositoryImpl {
+import com.example.bookshop.exception.DataProcessingException;
+import com.example.bookshop.model.Book;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class BookRepositoryImpl implements BookRepository {
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Override
+    public Book save(Book book) {
+        EntityManager entityManager = null;
+        EntityTransaction transaction = null;
+        try {
+            entityManager = entityManagerFactory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Book wasn't saved to db: " + book, e);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager.createQuery("FROM Book b", Book.class).getResultList();
+        }
+    }
 }
--- src/main/java/com/example/bookshop/service/BookServiceImpl.java
+++ src/main/java/com/example/bookshop/service/BookServiceImpl.java
@@ -1,5 +1,24 @@
 package com.example.bookshop.service;
 
-public class BookServiceImpl {
+import com.example.bookshop.model.Book;
+import com.example.bookshop.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
 }
