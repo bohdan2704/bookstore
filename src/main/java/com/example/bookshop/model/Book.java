cb30bae8e887b86edb11f967dd93f6ca34f161d2
Bohdan <bohdan2704@gmail.com>
2024-02-26 23:59:15 +0200

    added annotations

--- src/main/java/com/example/bookshop/model/Book.java
+++ src/main/java/com/example/bookshop/model/Book.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
