4e3d349eadd7fdc9fcbf2cd660707df2070f424d
Bohdan <148899164+bohdan2704@users.noreply.github.com>
2024-02-26 21:43:40 +0200

    Update src/main/java/com/example/bookshop/exception/DataProcessingException.java
    Co-authored-by: Leonid Sivko <rommel@ukr.net>

--- src/main/java/com/example/bookshop/exception/DataProcessingException.java
+++ src/main/java/com/example/bookshop/exception/DataProcessingException.java
@@ -1,10 +1,6 @@
 package com.example.bookshop.exception;
 
 public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String message) {
-        super(message);
-    }
-
     public DataProcessingException(String message, Throwable e) {
         super(message);
     }
