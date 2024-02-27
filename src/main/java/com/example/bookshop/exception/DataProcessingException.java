62f85a9e7e03d9a7e71c035fec099b1ace81ee83
Bohdan <148899164+bohdan2704@users.noreply.github.com>
2024-02-27 22:07:41 +0200

    updated exceptions throwing
    Co-authored-by: Kirill Koval <73041091+kerrrusha@users.noreply.github.com>

--- src/main/java/com/example/bookshop/exception/DataProcessingException.java
+++ src/main/java/com/example/bookshop/exception/DataProcessingException.java
@@ -2,6 +2,6 @@ package com.example.bookshop.exception;
 
 public class DataProcessingException extends RuntimeException {
     public DataProcessingException(String message, Throwable e) {
-        super(message);
+        super(message, e);
     }
 }
