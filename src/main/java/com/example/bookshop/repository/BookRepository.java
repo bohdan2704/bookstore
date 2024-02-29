075a42497515073635a9a94a98a159d6b14ec06c
Bohdan <bohdan2704@gmail.com>
2024-02-29 16:45:56 +0200

    fixed checkstyle

--- src/main/java/com/example/bookshop/repository/BookRepository.java
+++ src/main/java/com/example/bookshop/repository/BookRepository.java
@@ -1,8 +1,6 @@
 package com.example.bookshop.repository;
 
 import com.example.bookshop.model.Book;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
