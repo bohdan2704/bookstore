060c216f1a8341a4a46f9c098308576452b206a1
Bohdan <bohdan2704@gmail.com>
2024-03-01 16:57:23 +0200

    fixed checkstyle

--- src/main/java/com/example/bookshop/BookshopApplication.java
+++ src/main/java/com/example/bookshop/BookshopApplication.java
@@ -1,7 +1,5 @@
 package com.example.bookshop;
 
-import com.example.bookshop.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
