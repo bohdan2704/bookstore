4667a643859468f72c7882fd79bc363637783005
Bohdan <bohdan2704@gmail.com>
2024-03-01 16:56:45 +0200

    added cmn prefix, added records

--- src/main/java/com/example/bookshop/BookshopApplication.java
+++ src/main/java/com/example/bookshop/BookshopApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookshopApplication {
-    @Autowired
-    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(BookshopApplication.class, args);
