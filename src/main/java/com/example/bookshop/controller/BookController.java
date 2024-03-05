47ba9f4e5d53c741527cc3cd1bbb3aec18a2c97c
Bohdan <bohdan2704@gmail.com>
2024-03-05 12:38:05 +0200

    fixed liquibase chain and dependencies

--- src/main/java/com/example/bookshop/controller/BookController.java
+++ src/main/java/com/example/bookshop/controller/BookController.java
@@ -3,7 +3,6 @@ package com.example.bookshop.controller;
 import com.example.bookshop.dto.BookDto;
 import com.example.bookshop.dto.CreateBookDto;
 import com.example.bookshop.service.BookService;
-import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -32,12 +31,12 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody @Valid CreateBookDto bookDto) {
+    public BookDto createBook(@RequestBody CreateBookDto bookDto) {
         return bookService.save(bookDto);
     }
 
     @PutMapping(path = "/{id}")
-    public BookDto updateBook(@PathVariable Long id, @RequestBody @Valid CreateBookDto bookDto) {
+    public BookDto updateBook(@PathVariable Long id, @RequestBody CreateBookDto bookDto) {
         return bookService.updateById(id, bookDto);
     }
 
