1fd7dd729ba7395e7945392c45020733adc374a3
Bohdan <bohdan2704@gmail.com>
2024-03-05 12:53:32 +0200

    added exception handlers, validation

--- src/main/java/com/example/bookshop/controller/BookController.java
+++ src/main/java/com/example/bookshop/controller/BookController.java
@@ -3,6 +3,7 @@ package com.example.bookshop.controller;
 import com.example.bookshop.dto.BookDto;
 import com.example.bookshop.dto.CreateBookDto;
 import com.example.bookshop.service.BookService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -31,12 +32,12 @@ public class BookController {
     }
 
     @PostMapping
-    public BookDto createBook(@RequestBody CreateBookDto bookDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookDto bookDto) {
         return bookService.save(bookDto);
     }
 
     @PutMapping(path = "/{id}")
-    public BookDto updateBook(@PathVariable Long id, @RequestBody CreateBookDto bookDto) {
+    public BookDto updateBook(@PathVariable Long id, @RequestBody @Valid CreateBookDto bookDto) {
         return bookService.updateById(id, bookDto);
     }
 
